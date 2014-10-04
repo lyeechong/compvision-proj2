fae7019473a0c2fec83da15e9888a56af454fd70
Lyee Chong <lchong@cs.utexas.edu>
2014-10-04 15:10:09 -0500

    something

--- project_2/my_test.py
+++ project_2/my_test.py
@@ -10,4 +10,4 @@ import stereo
 image_a = cv2.imread('test_data/tsukuba/left.png')
 image_b = cv2.imread('test_data/tsukuba/right.png')
 
-stereo.find_fundamental_mat(image_a, image_b)
+stereo.rectify_pair(image_a, image_b)
