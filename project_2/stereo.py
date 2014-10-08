592a62b0f5a9c1829a73f11b7270abba40db9ef9
Timothy Sherlock <tsherloc@cs.utexas.edu>
2014-10-08 14:34:16 -0500

    passing all tests

--- project_2/stereo.py
+++ project_2/stereo.py
@@ -20,16 +20,20 @@ def rectify_pair(image_left, image_right, viz=False):
         their epipolar lines are corresponding rows.
     """
 
-    image_a_points, image_b_points = find_feature_points(image_left, image_right)
+    image_a_points, image_b_points = find_feature_points(image_left,
+                                                        image_right)
 
-    fundamental_mat, mask = cv2.findFundamentalMat(image_a_points, image_b_points, cv2.RANSAC)
+    fundamental_mat, mask = cv2.findFundamentalMat(image_a_points,
+                                image_b_points, cv2.RANSAC)
     imsize = (image_right.shape[1], image_right.shape[0])
     image_a_points = image_a_points[mask.ravel() == 1]
     image_b_points = image_b_points[mask.ravel() == 1]
-    
-    #rectification_transform, H1, H2 = cv2.stereoRectifyUncalibrated(image_a_points, image_b_points, fundamental_mat, imsize, np.zeros(9).reshape(3,3), np.zeros(9).reshape(3,3), 0.05)
-    rectification_transform, H1, H2 = cv2.stereoRectifyUncalibrated(image_a_points, image_b_points, fundamental_mat, imsize)
-    
+
+    rectification_transform, H1, H2 = cv2.stereoRectifyUncalibrated(
+                                        image_a_points,
+                                        image_b_points,
+                                        fundamental_mat, imsize)
+
     return fundamental_mat, H1, H2
 
 
@@ -44,27 +48,42 @@ def disparity_map(image_left, image_right):
         with respect to image_left's input pixels.
     """
     sbm = cv2.StereoSGBM()
-    sbm.SADWindowSize = 3;
+    sbm.SADWindowSize = 3
     sbm.numberOfDisparities = 96
-    sbm.preFilterCap = 0;
-    sbm.minDisparity = 16;
-    sbm.uniquenessRatio = 15;
-    sbm.speckleWindowSize = 100;
-    sbm.speckleRange = 32;
-    sbm.disp12MaxDiff = 1;
-    sbm.fullDP = False;
-    sbm.P1 = 8*image_left.shape[2]*(sbm.SADWindowSize**2)
-    sbm.P2 = 32*image_left.shape[2]*(sbm.SADWindowSize**2)
+    sbm.preFilterCap = 0
+    sbm.minDisparity = 16
+    sbm.uniquenessRatio = 15
+    sbm.speckleWindowSize = 100
+    sbm.speckleRange = 32
+    sbm.disp12MaxDiff = 1
+    sbm.fullDP = False
+    sbm.P1 = 8 * image_left.shape[2] * (sbm.SADWindowSize ** 2)
+    sbm.P2 = 32 * image_left.shape[2] * (sbm.SADWindowSize ** 2)
 
     disparity = sbm.compute(image_left, image_right)
-
     print type(disparity[0][0])
 
-    disparity_visual = cv2.normalize(disparity, alpha=0, beta=255, norm_type=cv2.cv.CV_MINMAX, dtype=cv2.cv.CV_8U)
-    cv2.imshow("sbm", disparity_visual)
-    cv2.waitKey(4000)
+    disparity_visual = cv2.normalize(disparity,
+        alpha=23, beta=50, norm_type=cv2.cv.CV_MINMAX,
+        dtype=cv2.cv.CV_8U)
+    #cv2.imshow("sbm", disparity_visual)
+    #cv2.waitKey(4000)
 
     return disparity_visual
+
+
+ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
+
 
 def point_cloud(disparity_image, image_left, focal_length):
     """Create a point cloud from a disparity image and a focal length.
@@ -79,7 +98,25 @@ def point_cloud(disparity_image, image_left, focal_length):
         pixels, with colors sampled from left_image. You may filter low-
         disparity pixels or noise pixels if you choose.
     """
-    pass
+
+    h, w = image_left.shape[:2]
+
+    Q = np.float32([[1, 0, 0, w / 2],
+                    [0, 1, 0, h / 2],
+                    [0, 0, focal_length, 0],
+                    [0, 0, 0, 1]])
+    points = cv2.reprojectImageTo3D(disparity_image, Q)
+    colors = cv2.cvtColor(image_left, cv2.COLOR_BGR2RGB)
+    mask = disparity_image > disparity_image.min()
+    out_points = points[mask]
+    out_colors = colors[mask]
+
+    verts = out_points.reshape(-1, 3)
+    colors = out_colors.reshape(-1, 3)
+    verts = np.hstack([verts, colors])
+
+    return ply_header % dict(vert_num=len(verts))
+
 
 def find_feature_points(image_a, image_b):
     sift = cv2.SIFT()
@@ -102,8 +139,7 @@ def find_feature_points(image_a, image_b):
         [kp_a[m.trainIdx].pt for (m, _) in matches]).reshape(-1, 1, 2)
     image_b_points = np.float32(
         [kp_b[m.queryIdx].pt for (m, _) in matches]).reshape(-1, 1, 2)
-    
-    # find fundamental mat    
+
+    # find fundamental mat
     return image_a_points, image_b_points
     #return np.int32(image_a_points), np.int32(image_b_points)
-    
