9d77f897fafa3020a5e03c6bfcfaa25024dca0a6
Tim Sherlock <the.tim.sherlock@gmail.com>
2014-10-14 21:30:24 -0500

    renamed files and saving disparity image

--- project_2/tim_stereo/my_stereo.py
+++ project_2/tim_stereo/my_stereo.py
@@ -1,22 +1,15 @@
 import stereo
 import cv2
 
-print 'start'
-
 left = cv2.imread('set_left.jpg')
 right = cv2.imread('set_right.jpg')
 
-print "shapes, ", left.shape, right.shape
-
 disparity = stereo.disparity_map(left, right)
-
-cv2.imshow('disparity', disparity)
-cv2.waitKey(5000)
+cv2.imwrite("set_disparity.jpg", disparity)
 
 colors = left
 focal_length = 5
-
 ply_string = stereo.point_cloud(disparity, colors, focal_length)
 
-with open("test.ply", 'w') as f:
+with open("set.ply", 'w') as f:
     f.write(ply_string)
